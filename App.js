66bb95e70600a7dfc80f3a97ddfa7edd46f0e69d
ramprasadselvam <ramprasadselvam@gmail.com>
2021-01-30 22:53:01 +0530

    First

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://reactnative.dev/movies.json')
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({ data: json.movies });
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+  }
+
+  render() {
+    const { data, isLoading } = this.state;
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {isLoading && data.length ? <ActivityIndicator /> :
+          data.map((item, i) => {
+            return <Text key={i}>{item.title}, {item.releaseYear}</Text>
+          })
+        }
+      </View>
+    );
+  }
+};
